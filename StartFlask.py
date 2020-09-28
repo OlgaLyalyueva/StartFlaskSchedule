80f7b165d8915390a05545dd07bb39029af1f711
OlgaLyalyueva <olgalyalyueva@gmail.com>
2020-09-28 13:44:08 +0300

    Add error message to login page

--- StartFlask.py
+++ StartFlask.py
@@ -2,7 +2,7 @@ from flask import Flask, session, redirect, url_for, request, render_template, f
 from flask_login import login_manager, LoginManager, login_user
 from markupsafe import escape
 from forms import RegistrationForm, LoginForm
-from models import User
+from models import User, generate_password_hash, check_password_hash
 from database import db_session, init_db
 from sqlalchemy.orm import query
 
@@ -53,39 +53,21 @@ def load_user(username):
     return User.query.filter_by(username=username).first()
 
 
-# @app.route('/login/', methods=['GET', 'POST'])
-# def login():
-#     form = LoginForm(request.form)
-#     if request.method == 'POST' and form.validate():
-#         if load_user(form.username.data) is None:
-#             flash('Invalid username')
-#             return render_template('login.html', title='Login', form=form)
-#         else:
-#             # first_name = load_user(form.username.data)
-#             return render_template('schedule.html')
-#     else:
-#         # if check_password_hash(cred.password, form.password.data):
-#         #     return redirect(url_for('browse'))
-#         # else:
-#             flash('Invalid password')
-#             return render_template('login.html', title='Login', form=form)
-#     return render_template('login.html', title='Login', form=form)
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm(request.form)
+    error = None
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        u = User.query.filter_by(username=username, password=password).first()
-        print(username, password, u)
-        if u is not None:
-            return schedule()
+        u = User.query.filter_by(username=username).first()
+        if u is not None and check_password_hash(u.password, password):
+            session['user_id'] = u.user_id
+            return redirect(url_for('main'))
         else:
             error = 'Неправильный логин или пароль'
             return render_template('login.html', form=form, error=error)
-    return render_template('login.html', form=form)
+    return render_template('login.html', form=form, error=error)
 
 
 @app.route('/registration', methods=['GET', 'POST'])
@@ -98,38 +80,9 @@ def registration():
         db_session.commit()
         flash('Thanks for registering')
         return redirect(url_for('login'))
+
     return render_template('registration.html', form=form)
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     error = None
-#     if request.method == 'POST':
-#         if valid_login(request.form['username'],
-#                        request.form['password']):
-#             return log_the_user_in(request.form['username'])
-#         else:
-#             error = 'Invalid username/password'
-#             return render_template('login.html', error=error)
-#     else:
-#         return show_the_registration_form()
-#
-#
-# def valid_login(username, password):
-#     if username == User.login:
-#         login_user_id = User.get.id(login == username)
-#         if password == User.password:
-#             password_user_id = User.get.id(password == password)
-#             if login_user_id == password_user_id:
-#                 return 'True'
-#
-#
-# @app.route('/logged/username')
-# def log_the_user_in(username):
-#     return render_template('logged.html', username)
-#
-#
-# def show_the_registration_form():
-#     return render_template('registration.html')
 
 
 
@@ -146,3 +99,6 @@ def page_not_found(error):
 # def show_user_profile(username):
 #     return 'Пользователь %s' % escape(username)
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
